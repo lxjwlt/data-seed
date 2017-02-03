7b85cd84a9f92040c63a8127732e4884e7910726
lxjwlt <lxjwlt.c@gmail.com>
2017-02-03 20:36:44 +0800

    ip4 cidr seed

--- src/seed/ip4.js
+++ src/seed/ip4.js
@@ -3,6 +3,12 @@
 const random = require('../util/random');
 const arr = require('../util/arr');
 
-module.exports = () => {
+let ip4 = () => {
     return arr(4, () => random.int(0, 255)).join('.');
 };
+
+ip4.cidr = () => {
+    return ip4() + '/' + random.int(1, 32);
+};
+
+module.exports = ip4;
--- tests/src/seed/ip4.js
+++ tests/src/seed/ip4.js
@@ -18,4 +18,28 @@ describe('seed/ip4.js', () => {
         assert.isTrue(validator.isIP(ip4Seed(), 4));
     });
 
+    describe('@cidr', () => {
+
+        it('should be string', () => {
+            assert.isString(ip4Seed.cidr());
+        });
+
+        it('should be diff', () => {
+            assert.notStrictEqual(ip4Seed.cidr(), ip4Seed.cidr());
+        });
+
+        it('should be right format', () => {
+            let cidr = ip4Seed.cidr().split('/');
+            let ip = cidr[0];
+            let segment = cidr[1];
+
+            assert.isTrue(validator.isIP(ip, 4));
+            assert.isTrue(validator.isInt(segment, {
+                min: 1,
+                max: 32
+            }));
+        });
+
+    });
+
 });
