79069d061f70fbad8e92f4f72ff2da3f11fd5f4e
lxjwlt <lxjwlt.c@gmail.com>
2017-02-04 17:22:08 +0800

    hex color should length 6

--- src/seed/hex-color.js
+++ src/seed/hex-color.js
@@ -5,9 +5,11 @@ let random = require('../util/random');
 function hexColor () {
     let startPoint = 0;
     let endPoint = parseInt('ffffff', 16);
-    let hex = random.int(startPoint, endPoint);
+    let hex = random.int(startPoint, endPoint).toString(16);
 
-    return '#' + hex.toString(16);
+    hex = '0'.repeat(6 - hex.length) + hex;
+
+    return '#' + hex;
 }
 
 module.exports = hexColor;
--- tests/src/seed/hex-color.js
+++ tests/src/seed/hex-color.js
@@ -1,15 +1,13 @@
 'use strict';
 
 const assert = require('chai').assert;
+const validator = require('validator');
 const hexColorSeed = require('../../../src/seed/hex-color');
 
 describe('seed/hex-color.js', () => {
 
-    it('should be string hex value', () => {
-        let hex = hexColorSeed();
-
-        assert.isString(hex);
-        assert.notStrictEqual(hex.match(/^#[0-9a-f]{6}$/), null);
+    it('should be hex color', () => {
+        assert.isTrue(validator.isHexColor(hexColorSeed()));
     });
 
 });
