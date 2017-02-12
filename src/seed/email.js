63d6c3f128fe750432e7e3fae2d54c2d3c99d6a7
lxjwlt <lxjwlt.c@gmail.com>
2017-02-12 18:57:34 +0800

    email seed bug : return xxx@xx..xx.com

--- src/seed/email.js
+++ src/seed/email.js
@@ -6,7 +6,7 @@ const wordSeed = require('./word');
 const {wrap} = require('../util/hooks');
 
 let email = wrap(function () {
-    return wordSeed() + '@' + arr(random.int(2, 4), wordSeed).join('.');
+    return wordSeed() + '@' + arr(random.int(2, 4), () => wordSeed()).join('.');
 });
 
 module.exports = email;
