f1600d9a541db3a53ead15fe4dc2925b09fe3f53
lxjwlt <lxjwlt.c@gmail.com>
2017-02-15 23:14:16 +0800

    util.gather test

--- tests/src/util/gather.js
+++ tests/src/util/gather.js
@@ -5,7 +5,6 @@ const gather = require('../../../src/util/gather');
 
 describe('util/gather.js', () => {
 
-
     it('next item in array', () => {
         let arr = [1, 2, 3];
         let list = gather(arr);
@@ -46,5 +45,72 @@ describe('util/gather.js', () => {
 
     });
 
+    it('iterator arr', () => {
+        let arr = [1, 2, 3];
+        let list = gather(arr);
+        let result = [];
+
+        for (let value of list) {
+            assert.include(arr, value);
+            result.push(value);
+        }
+
+        assert.notStrictEqual(result[0], result[1]);
+        assert.notStrictEqual(result[1], result[2]);
+        assert.notStrictEqual(result[0], result[2]);
+
+    });
+
+    it('next item in object', () => {
+        let obj = {
+            item1: 1,
+            item2: 2,
+            item3: 3
+        };
+        let list = gather(obj);
+        let result = [];
+
+        for (let item of list) {
+            assert.deepEqual(Object.assign({}, obj, item), obj);
+            result.push(item);
+        }
+
+        assert.notDeepEqual(result[0], result[1]);
+        assert.notDeepEqual(result[1], result[2]);
+        assert.notDeepEqual(result[0], result[2]);
+
+    });
+
+    it('empty array', () => {
+        let list = gather([]);
+
+        assert.strictEqual(list.next(), undefined);
+        assert.strictEqual(list.next(), undefined);
+
+    });
+
+    it('empty obj', () => {
+        let list = gather({});
+
+        assert.strictEqual(list.next(), undefined);
+        assert.strictEqual(list.next(), undefined);
+
+    });
+
+    it('no iterator in empty array', () => {
+
+        for (let value of gather([])) {
+            assert.strictEqual(1, 2);
+        }
+
+    });
+
+    it('no iterator in empty obj', () => {
+
+        for (let value of gather({})) {
+            assert.strictEqual(1, 2);
+        }
+
+    });
 
 });
